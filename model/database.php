b77ca1378370a755837fecfc9574b9fb4590c71f
305WEB <CONTACT@CLAUDIOABBASCIA.COM>
2024-03-05 13:41:44 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -2,7 +2,7 @@
 //data source network
 $dsn = "mysql:host=e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=u3pvxh30ckoo5wx6";
 $username = 'sjuho1vdhx8piddg';
-$password = "t7rht7z2mxlqjwyq";
+$password = 't7rht7z2mxlqjwyq';
 
 try {
     $db = new PDO($dsn, $username);
